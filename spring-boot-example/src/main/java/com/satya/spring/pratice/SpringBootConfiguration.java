69728de1330cad3396d172d1908e32719e8eb68a
satya2306 <satyabirapradhan@yahoo.co.in>
2017-09-01 01:22:28 +0530

    Added webservice

--- spring-boot-example/src/main/java/com/satya/spring/pratice/SpringBootConfiguration.java
+++ spring-boot-example/src/main/java/com/satya/spring/pratice/SpringBootConfiguration.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication  
 
-public class SpringBootExample {
+public class SpringBootConfiguration {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		SpringApplication.run(SpringBootExample.class, args);
+		SpringApplication.run(SpringBootConfiguration.class, args);
 	}
 
 }
---
+++ spring-boot-example/src/test/java/com/satya/spring/pratice/HelloWorldControllerTest.java
@@ -0,0 +1,71 @@
+package com.satya.spring.pratice;
+
+import static org.junit.Assert.*;
+
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringBootConfiguration.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = {"management.port=0"})
+public class HelloWorldControllerTest {
+
+	@LocalServerPort
+	private int port;
+
+	@Value("${local.management.port}")
+	private int mgt;
+
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+
+	@Test
+	public void shouldReturn200WhenSendingRequestToController() throws Exception {
+		@SuppressWarnings("rawtypes")
+		ResponseEntity<Map> entity = this.testRestTemplate.getForEntity(
+				"http://localhost:" + this.port + "/hello-world", Map.class);
+
+		then(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
+	
+	@Test
+	public void shouldReturn200WhenSendingRequestToControllerJ() throws Exception {
+		@SuppressWarnings("rawtypes")
+		ResponseEntity<Map> entity = this.testRestTemplate.getForEntity(
+				"http://localhost:" + this.port + "/hello-world", Map.class);
+		Assert.assertEquals(entity.getStatusCode(), HttpStatus.OK);
+	}
+		
+		@Test
+		public void shouldReturWhenSendingRequestToController() throws Exception {
+			@SuppressWarnings("rawtypes")
+			ResponseEntity<Map> entity = this.testRestTemplate.getForEntity(
+					"http://localhost:" + this.port + "/helloworld", Map.class);
+			Assert.assertEquals(entity.getStatusCode(), HttpStatus.NOT_FOUND);
+
+		//then(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
+
+	@Test
+	public void shouldReturn200WhenSendingRequestToManagementEndpoint() throws Exception {
+		@SuppressWarnings("rawtypes")
+		ResponseEntity<Map> entity = this.testRestTemplate.getForEntity(
+				"http://localhost:" + this.mgt + "/info", Map.class);
+
+		then(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
+}
